cd2b6b73e3dc92a56f29dcba5090f31413524749
miteshshelar1 <134475465+miteshshelar1@users.noreply.github.com>
2023-05-24 11:19:18 +0530

    first commit

---
+++ my-app11/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { CenterComponent } from './center/center.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CenterComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-app11/src/app/center/center.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-center',
+  templateUrl: './center.component.html',
+  styleUrls: ['./center.component.css']
+})
+export class CenterComponent {
+
+  data =[
+    {"title":"Adidas","subtitle":"Casual","img":"../../assets/mens/mens shoe/1.jpg","disc":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, modi. Earum dolore","icon":3,"price":900,},
+    {"title":"Crocs","subtitle":"Casual","img":"../../assets/mens/mens shoe/2.jpg","disc":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, modi. Earum dolore","price":400},
+    {"title":"ASICS","subtitle":"Casual","img":"../../assets/mens/mens shoe/3.jpg","disc":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, modi. Earum dolore","price":300},
+    {"title":"Sketures","subtitle":"Casual","img":"../../assets/mens/mens shoe/4.jpg","disc":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, modi. Earum dolore","price":200},
+    {"title":"ASICS","subtitle":"Casual","img":"../../assets/mens/mens shoe/5.jpg","disc":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, modi. Earum dolore","price":500},
+    {"title":"Adidas","subtitle":"Casual","img":"../../assets/mens/mens shoe/6.jpg","disc":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, modi. Earum dolore","price":700},
+    {"title":"Crocks","subtitle":"Casual","img":"../../assets/mens/mens shoe/7.jpg","disc":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, modi. Earum dolore","price":900},
+    {"title":"Sketures","subtitle":"Casual","img":"../../assets/mens/mens shoe/8.jpg","disc":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, modi. Earum dolore","price":600},
+  ]
+
+
+}
